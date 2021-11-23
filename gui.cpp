1a3c1b547cf61efaf7e143d0f39e163690e6547e
Rory652 <el20rdc@leeds.ac.uk>
2021-11-23 23:03:17 +0000

    Added files for GUI

---
+++ gui.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Rory on 23/11/2021.
+//
+
+#include "gui.h"
+
+void printHeader(std::string header) {
+    std::cout << getLine(10) << " " << header << " " << getLine(10) << std::endl;
+}
+
+/* Planned functions:
+    * + - / ()
+    Trigonometric functions: sin, cos, tan
+    Logarithm: ln, log_
+    Exponential: e^, n^
+    Powers: x^, sqrt(x)
+*/
+void printHelp() {
+
+}
+
+std::string getLine(int n) {
+    std::string str;
+    for (int i = 0; i < n; i++) {
+        str.append("-");
+    }
+    return str;
+}
---
+++ gui.h
@@ -0,0 +1,16 @@
+//
+// Created by Rory on 23/11/2021.
+//
+
+#ifndef GUI_H
+#define GUI_H
+
+#include <string>
+#include <iostream>
+
+void printHeader(std::string header);   // Print a header with the string giver
+void printHelp();                       // Print the function help guide
+
+std::string getLine(int n);             // Returns a line of hyphens "n" long
+
+#endif //GUI_H
