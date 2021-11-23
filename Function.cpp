eb7a5501b28a36ff27bca7ce5199a1bca5df3a40
Rory652 <el20rdc@leeds.ac.uk>
2021-11-23 17:07:44 +0000

    Initial Class Setup

--- Function.cpp
+++ Function.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Function.h"
+
+#include <utility>
+
+Function::Function() {
+    inStr = "x";
+    terms = 1;
+    lower = 0.0;
+    upper = 1.0;
+}
+
+Function::Function(std::string &in, int n, double l, double u) {
+    inStr = in;
+    terms = n;
+    lower = l;
+    upper = u;
+}
--- Function.h
+++ Function.h
@@ -5,9 +5,23 @@
 #ifndef INC_2645_FUNCTION_H
 #define INC_2645_FUNCTION_H
 
+#include <string>
+#include <vector>
 
 class Function {
-
+    public:
+        // Default Constructor
+        Function();
+        // Expected Constructor
+        Function(std::string &in, int n, double l, double u);
+    private:
+        // Input Values
+        std::string inStr;
+        int terms;
+        double lower, upper;
+        // Output Values
+        double a0 = 0;
+        std::vector<double> a_n, b_n;
 };
 
 
