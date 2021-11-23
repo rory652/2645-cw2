bc7cd105f951c03ca50b2cc6bb15898936f33667
Rory652 <el20rdc@leeds.ac.uk>
2021-11-23 16:58:39 +0000

    Initial Commit

---
+++ Function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rory on 23/11/2021.
+//
+
+#include "Function.h"
---
+++ Function.h
@@ -0,0 +1,14 @@
+//
+// Created by Rory on 23/11/2021.
+//
+
+#ifndef INC_2645_FUNCTION_H
+#define INC_2645_FUNCTION_H
+
+
+class Function {
+
+};
+
+
+#endif //INC_2645_FUNCTION_H
